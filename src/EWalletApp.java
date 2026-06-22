bf91bfe43ddd1dd7683d9330560fe3a4df5d54af
Jazzmyn Cruz <graphicperfectity@gmail.com>
2026-06-22 01:53:56 -0400

    Implemented the expense storage and array list

--- src/EWalletApp.java
+++ src/EWalletApp.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 
-public class EWalletApp
+public class EWalletApp implements Expenser
 {
 
 	//this is the app class, has the GUI and create one object of your expense calculator class. The expense calculator class is the implementation of the Expenser interface 
@@ -13,16 +13,23 @@ public class EWalletApp
 	private ArrayList<User> AllData;
 
 	private ArrayList<Wage> incomes = new ArrayList<>();
-    
+    private ArrayList<Expense> expenses = new ArrayList<>();
 
 	public void CreateUser(String username, String password) 
 	{
 		
 	}
 
+	@Override
 	public void addMonthlyIncome(Wage W) 
 	{
    		incomes.add(W);
+	}
+
+	@Override
+	public void addExpense(Expense Ex) 
+	{
+   		expenses.add(Ex);
 	}
 
 
